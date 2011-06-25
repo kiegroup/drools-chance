b4680ce5e64b65e954ee2951917d0073acd19f53
Davide Sottara <dsotty@gmail.com>
2011-06-25 20:46:37 +0200

    Partial Upgrade to 5.3 and towards conforming with Drools pom structure

--- drools-pmml/drools-pmml-test/src/test/java/org/drools/pmml_4_0/global/TestConstrainedDataDictionary.java
+++ drools-pmml/drools-pmml-test/src/test/java/org/drools/pmml_4_0/global/TestConstrainedDataDictionary.java
@@ -23,7 +23,7 @@ import java.util.Collection;
 public class TestConstrainedDataDictionary extends DroolsAbstractPMMLTest {
 
     private static final boolean VERBOSE = true;
-    private static final String source = "test_constr_data_dic.xml";
+    private static final String source = "org/drools/pmml_4_0/test_constr_data_dic.xml";
     private static final String packageName = "org.drools.pmml_4_0.test";
 
 
