b5126a338336718a93e9d87fb30f1bb8c00f19ed
Davide Sottara <dsotty@gmail.com>
2012-02-22 16:24:48 +0100

    (Human) Task-based interactions

--- drools-informer/drools-informer-interaction/src/test/java/TaskInteractionTest.java
+++ drools-informer/drools-informer-interaction/src/test/java/TaskInteractionTest.java
@@ -14,35 +14,42 @@
  * limitations under the License.
  */
 
-import org.drools.ClassObjectFilter;
-import org.drools.KnowledgeBase;
-import org.drools.KnowledgeBaseFactory;
+import org.drools.*;
 import org.drools.builder.KnowledgeBuilder;
 import org.drools.builder.KnowledgeBuilderFactory;
 import org.drools.builder.ResourceType;
+import org.drools.conf.AssertBehaviorOption;
+import org.drools.conf.EventProcessingOption;
 import org.drools.definition.type.FactType;
 import org.drools.informer.Answer;
 import org.drools.informer.MultipleChoiceQuestion;
 import org.drools.io.impl.ClassPathResource;
+import org.drools.runtime.KnowledgeSessionConfiguration;
 import org.drools.runtime.StatefulKnowledgeSession;
+import org.drools.runtime.conf.ClockTypeOption;
 import org.drools.runtime.rule.Variable;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 
-public class HumanTaskTest {
+public class TaskInteractionTest {
 
 
     StatefulKnowledgeSession kSession;
-    String drl1 = "org/drools/informer/interaction/interaction.drl";
-    String drl2 = "org/drools/informer/interaction/humanTask.drl";
+    String drl1 = "org/drools/informer/interaction/humanTask.drl";
+    String drl2 = "org/drools/informer/interaction/interaction.drl";
+    String drl3 = "org/drools/informer/interaction/quests.drl";
 
-    String drl3 = "org/drools/informer/interaction/humanTaskTest.drl";
+    String testDrl = "org/drools/informer/interaction/interactionTest.drl";
 
 
     @Before
@@ -53,16 +60,284 @@ public class HumanTaskTest {
         kBuilder.add( new ClassPathResource( drl1 ), ResourceType.DRL );
         kBuilder.add( new ClassPathResource( drl2 ), ResourceType.DRL );
         kBuilder.add( new ClassPathResource( drl3 ), ResourceType.DRL );
+        kBuilder.add( new ClassPathResource( testDrl ), ResourceType.DRL );
 
         if ( kBuilder.hasErrors() ) {
             fail( kBuilder.getErrors().toString() );
         }
-        KnowledgeBase kBase = KnowledgeBaseFactory.newKnowledgeBase();
+
+        KnowledgeBaseConfiguration kbconf = KnowledgeBaseFactory.newKnowledgeBaseConfiguration();
+        kbconf.setOption( AssertBehaviorOption.EQUALITY );
+        kbconf.setOption( EventProcessingOption.STREAM );
+        KnowledgeBase kBase = KnowledgeBaseFactory.newKnowledgeBase( kbconf );
         kBase.addKnowledgePackages( kBuilder.getKnowledgePackages() );
-        kSession = kBase.newStatefulKnowledgeSession();
+
+        KnowledgeSessionConfiguration ksconf = KnowledgeBaseFactory.newKnowledgeSessionConfiguration();
+        ksconf.setProperty(ClockTypeOption.PROPERTY_NAME, ClockType.REALTIME_CLOCK.toExternalForm());
+        KnowledgeSessionConfiguration ksessionConf = KnowledgeBaseFactory.newKnowledgeSessionConfiguration();
+        kSession = kBase.newStatefulKnowledgeSession( ksessionConf, null );
+
+        kSession.setGlobal( "list", new ArrayList() );
+        kSession.setGlobal( "taskLog", new ArrayList() );
+
         kSession.fireAllRules();
 
     }
+
+
+
+
+    //TODO : Overarching task (interaction) with connected surveys
+
+    //TODO : Overarching task without connected surveys, but externally triggered actions
+
+
+
+
+
+    @Test
+    public void testSurveyTaskWithInteractionWithoutAlerting() {
+
+        kSession.insert( "surveytaskinter" );
+        kSession.fireAllRules();
+
+
+        String mainTaskId = getMainTaskQuestId( kSession );
+        String questionId = getTransitionQuestion( kSession, mainTaskId );
+
+        System.out.println( "@@@@@@@@@@@" + questionId );
+
+        kSession.insert( new Answer( "transition", mainTaskId, "START" ) );
+        kSession.fireAllRules();
+
+        kSession.insert( new Answer( "question2", getSurveyId( "SurveyBean" ), "someValue" ) );
+        kSession.fireAllRules();
+
+
+
+        report( kSession, System.err );
+
+//        assertEquals( Arrays.asList( "actor1", "actor2" ), kSession.getGlobal( "list" ) );
+
+        System.err.println( kSession.getGlobal( "list" ) );
+        System.err.println( kSession.getGlobal( "taskLog" ) );
+
+        assertEquals( 8, ((List) kSession.getGlobal( "taskLog" )).size() );
+        assertEquals( 0, kSession.getObjects().size() );
+
+        kSession.dispose();
+    }
+
+
+    private String getTransitionQuestion(StatefulKnowledgeSession kSession, String mainTaskId) {
+        return (String) kSession.getQueryResults( "getItemId", "transition", mainTaskId, Variable.v ).iterator().next().get( "$id" );
+//        return "";
+    }
+
+
+    private String getSurveyId( String className ) {
+        FactType sKlass = kSession.getKnowledgeBase().getFactType( "org.drools.informer.interaction", className );
+        Collection beans = kSession.getObjects( new ClassObjectFilter( sKlass.getFactClass() ) );
+        Object bean = beans.iterator().next();
+        return (String) sKlass.get( bean, "questionnaireId" );
+    }
+
+
+    private String getMainTaskQuestId(StatefulKnowledgeSession kSession) {
+        FactType itKlass = kSession.getKnowledgeBase().getFactType( "org.drools.informer.interaction", "InteractiveTask" );
+        Collection interTasks = kSession.getObjects( new ClassObjectFilter( itKlass.getFactClass() ) );
+        assertEquals( 1, interTasks.size() );
+        Object mainTask = interTasks.iterator().next();
+        return (String) itKlass.get( mainTask, "controlQuestId" );
+    }
+
+
+    @Test
+    public void testInteractionWithDelegation() {
+
+
+        Thread slave = new Thread() {
+            public void run() {
+                kSession.insert( "delegation test" );
+                kSession.fireUntilHalt();
+            }
+        };
+
+        slave.start();
+
+
+        try {
+            Thread.sleep( 3000 );
+        } catch (InterruptedException e) {
+            fail();
+        }
+
+        kSession.halt();
+
+        try {
+            slave.join();
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+
+        report( kSession, System.err );
+
+        assertEquals( 0, kSession.getObjects().size() );
+        assertEquals( Arrays.asList( "actor1", "actor2" ), kSession.getGlobal( "list" ) );
+
+        assertEquals( 6, ((List) kSession.getGlobal( "taskLog" )).size() );
+
+
+        System.err.println( kSession.getGlobal( "list" ) );
+        System.err.println( kSession.getGlobal( "taskLog" ) );
+
+        kSession.dispose();
+    }
+
+
+
+
+    @Test
+    /**
+     * Use case:
+     *  where a Q is associated to a task.
+     *  The Q is removed before the task is completed,
+     *  so the task has to be updated accordingly
+     */
+    public void testSurveyTaskCancellation() {
+
+        kSession.insert( "surveytask" );
+        kSession.fireAllRules();
+
+        kSession.insert( "removesurvey" );
+        kSession.fireAllRules();
+
+
+        report( kSession, System.err );
+        System.err.println( kSession.getGlobal( "list" ) );
+        System.err.println( kSession.getGlobal( "taskLog" ) );
+
+        assertEquals( 0, kSession.getObjects().size() );
+
+        assertEquals( 4, ((List) kSession.getGlobal( "taskLog" )).size() );
+
+        kSession.dispose();
+    }
+
+
+
+
+    @Test
+    /**
+     * Use case:
+     *  simple questionnaire, without any associated task
+     *  The questionnaire is used to convey info into the WM
+     *  The survey can also show info to the user.
+     *
+     *  The Q "lives" even after being completed
+     */
+    public void testStatelessSurvey() {
+
+        kSession.insert( "survey" );
+        kSession.fireAllRules();
+
+        kSession.insert( new Answer( "question2", "testId", "someValue" ) );
+        kSession.fireAllRules();
+
+
+        report( kSession, System.err );
+        assertEquals( 15, kSession.getObjects().size() );
+
+        System.err.println( kSession.getGlobal( "list" ) );
+        System.err.println( kSession.getGlobal( "taskLog" ) );
+
+
+        assertEquals( 0, ((List) kSession.getGlobal( "taskLog" )).size() );
+        assertEquals( 0, ((List) kSession.getGlobal( "list" )).size() );
+
+
+        kSession.dispose();
+    }
+
+
+
+    @Test
+    /**
+     * Use case:
+     *  "stateful" questionnaire + task
+     *  The task monitors the Q's progress, which generates events
+     *  to drive the task state
+     */
+    public void testSurveyTask() {
+
+        kSession.insert( "surveytask" );
+        kSession.fireAllRules();
+
+        kSession.insert( new Answer( "question2", "testId", "someValue" ) );
+        kSession.fireAllRules();
+
+
+        report( kSession, System.err );
+        System.err.println( kSession.getGlobal( "list" ) );
+        System.err.println( kSession.getGlobal( "taskLog" ) );
+
+        assertEquals( 0, kSession.getObjects().size() );
+
+        assertEquals( 4, ((List) kSession.getGlobal( "taskLog" )).size() );
+
+
+        kSession.dispose();
+    }
+
+
+
+
+
+
+
+
+
+
+    @Test
+    public void testInteraction() {
+
+
+        Thread slave = new Thread() {
+            public void run() {
+                kSession.insert( "interaction test" );
+                kSession.fireUntilHalt();
+            }
+        };
+
+        slave.start();
+
+
+        try {
+            Thread.sleep( 5000 );
+        } catch (InterruptedException e) {
+            fail();
+        }
+
+        kSession.halt();
+
+        try {
+            slave.join();
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+
+        report( kSession, System.err );
+        assertEquals( 0, kSession.getObjects().size() );
+
+        System.err.println( kSession.getGlobal( "list" ) );
+        System.err.println( kSession.getGlobal( "taskLog" ) );
+
+        assertEquals(Arrays.asList("actor1", "main1", "actor2", "alien2", "actor3"), kSession.getGlobal("list"));
+        assertEquals( 14, ((List) kSession.getGlobal( "taskLog" )).size() );
+
+        kSession.dispose();
+    }
+
 
     @Test
     public void testTaskStateTransitionByQuestionnaire() {
@@ -77,7 +352,7 @@ public class HumanTaskTest {
         Object iTxHolder = kSession.getObjects( new ClassObjectFilter( txHolderClass.getFactClass() ) ).iterator().next();
 
         String taskId = (String) taskClass.get( iTask, "taskId" );
-        String tsId = (String) taskClass.get( iTask, "surveyableTxFId" );
+        String tsId = (String) taskClass.get( iTask, "controlQuestId" );
         assertNotNull( taskId );
         assertNotNull( tsId );
         assertEquals( tsId, txHolderClass.get( iTxHolder, "questionnaireId" ) );
@@ -95,7 +370,7 @@ public class HumanTaskTest {
         assertEquals( 4, transitions.getNumOfPossibleAnswers() );
         assertArrayEquals( new String[] { "ACTIVATE", "EXIT", "SKIP", "ERROR" }, transitions.getPossibleAnswersValues() );
 
-//        report( kSession, System.err );
+//
         System.out.println("-----");
         kSession.insert( new Answer( "transition", tsId, "ACTIVATE" ) );
         kSession.fireAllRules();
@@ -109,7 +384,7 @@ public class HumanTaskTest {
 
         kSession.insert( new Answer( "transition", tsId, "SUSPEND" ) );
         kSession.fireAllRules();
-
+        report( kSession, System.err );
         assertEquals( "SUSPENDED_READY", taskClass.get( iTask, "state" ).toString() );
         assertEquals( null, taskClass.get( iTask, "owner" ) );
         assertArrayEquals( new String[] { "RESUME" }, transitions.getPossibleAnswersValues());
@@ -219,8 +494,9 @@ public class HumanTaskTest {
         kSession.fireAllRules();
 
         assertEquals( "IN_PROGRESS", taskClass.get( iTask, "state" ).toString() );
-        assertEquals( "dsotty", taskClass.get( iTask, "owner" ) );
-        assertArrayEquals( new String[] { "SUSPEND", "STOP", "COMPLETE", "FAIL", "DELEGATE", "REVOKE", "FORWARD", "EXIT", "SKIP", "ERROR" }, transitions.getPossibleAnswersValues());
+        assertEquals( "dsotty", taskClass.get(iTask, "owner") );
+        System.out.println( Arrays.asList(transitions.getPossibleAnswersValues()) );
+        assertArrayEquals(new String[]{"SUSPEND", "STOP", "COMPLETE", "FAIL", "DELEGATE", "REVOKE", "FORWARD", "EXIT", "SKIP", "ERROR"}, transitions.getPossibleAnswersValues());
 
 
         kSession.insert( new Answer( "owner", tsId, "davide" ) );
@@ -307,7 +583,7 @@ public class HumanTaskTest {
         Object iTxHolder = kSession.getObjects( new ClassObjectFilter( txHolderClass.getFactClass() ) ).iterator().next();
 
         String taskId = (String) taskClass.get( iTask, "taskId" );
-        String tsId = (String) taskClass.get( iTask, "surveyableTxFId" );
+        String tsId = (String) taskClass.get( iTask, "controlQuestId" );
         assertNotNull( taskId );
         assertNotNull( tsId );
         assertEquals( tsId, txHolderClass.get( iTxHolder, "questionnaireId" ) );
@@ -322,8 +598,8 @@ public class HumanTaskTest {
         assertEquals( true, taskClass.get( iTask, "surveyableTx" ) );
         assertEquals( false, taskClass.get( iTask, "surveyableState" ) );
         assertEquals( 0, owners.getNumOfPossibleAnswers() );
-        assertEquals( 1, transitions.getNumOfPossibleAnswers() );
-        assertArrayEquals( new String[] { "START" }, transitions.getPossibleAnswersValues() );
+        assertEquals( 4, transitions.getNumOfPossibleAnswers() );
+        assertArrayEquals( new String[] { "START", "EXIT", "SKIP", "ERROR" }, transitions.getPossibleAnswersValues() );
 
 
         kSession.insert( new Answer( "transition", tsId, "START" ) );
@@ -331,7 +607,7 @@ public class HumanTaskTest {
 
         assertEquals( "IN_PROGRESS", taskClass.get( iTask, "state" ).toString() );
         assertEquals( "davide", taskClass.get( iTask, "owner" ) );
-        assertArrayEquals( new String[] { "STOP", "COMPLETE", "FAIL" }, transitions.getPossibleAnswersValues());
+        assertArrayEquals( new String[] { "STOP", "COMPLETE", "FAIL", "EXIT", "SKIP", "ERROR" }, transitions.getPossibleAnswersValues());
 
 
         kSession.insert( new Answer( "transition", tsId, "COMPLETE" ) );
@@ -360,5 +636,12 @@ public class HumanTaskTest {
         }
         out.println(" -------------------------------- " + kSession.getObjects().size() + " ----------------------------" );
     }
+
+
+
+
+
+
+
 
 }
